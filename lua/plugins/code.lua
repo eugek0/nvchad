9a7d7a76689951c42b91ce68cf03301ad3a455f8
eugek0 <jmaster21042005@mail.ru>
2025-09-19 09:33:29 +0300

    фикс плагина подсветки метод

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -7,7 +7,10 @@ return {
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    event = "VeryLazy",
+    config = function()
+      require("todo-comments").setup()
+    end,
+    event = "BufReadPost",
   },
 
   {
